818bd7e87e81fc8e490accd96db19e06bd9c617c
YanaPatyuk <yanch22@gmail.com>
2017-12-10 16:28:40 +0200

    main try2

--- ex4/src/client/src/main.cpp
+++ ex4/src/client/src/main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <limits>
 #include <fstream>
-#include <algorithm>
 #include <string>
 #include "../include/Board.h"
 #include <sstream>
@@ -28,6 +27,7 @@ int main() {
     PlayerInterface *p2 = NULL;
     short userChoice;
     bool correct = false;
+    bool server = false;
     cout << "Let The Game Begin!" << endl;
     cout << "May the odds be ever in your favor\n" << endl;
     do {//ask user against who he wants to play.
@@ -48,52 +48,64 @@ int main() {
             p1 = new TerminalPlayer(EnumDeclration::X);
             p2 = new AiPlayer(EnumDeclration::O);
         } else if (userChoice == 3) {//play against remote player.
-            // Read IP and port of remote Host from file
-            string ip, line;
-            int port;
-            ifstream hostInfo("hostInfo.txt");
-            if (hostInfo.is_open()) {
-                // break the first line into IP
-                // break the second line to Port No.
-                string delimiter = ":";
-                getline(hostInfo, line);
-                string token = line.substr(line.find(delimiter)+1, line.length());
-                // remove spaces from the ip/port
-                std::remove(token.begin(), token.end(), ' ');
-                ip = token;
-                getline(hostInfo, line);
-                token = line.substr(line.find(delimiter)+1, line.length());
-                std::istringstream(token) >> port; //convert to int
-                hostInfo.close();
-            } else {
-                cout << "Unable to open file" << endl;
+            /**
+             * Read IP and port of remote Host from file
+             */
+
+            /**FILE *hostInfo = fopen("hostInfo.txt", "rb");
+            char buffer[BUFFSIZE];
+            size_t bytes;
+            if (hostInfo == NULL) {
+                // Failed opening file...
+                return 0;
             }
-            //HostPlayer p3("127.0.0.1", 8000); DEFAULT
-            cout << "ip read from file: " << ip << endl;
-            cout << "port read from file: " << port << endl;
-            HostPlayer *p3 = new HostPlayer(ip.c_str(), port);
+            // as long as there bytes to copy copy them
+            while ((bytes = fread(buffer, 1, BUFFSIZE, hostInfo)) != 0) {
+            }
+            fclose(hostInfo);
+string ipNumber;
+            str
+            string line;
+            ifstream hostInfo("hostInfo.txt");
+            if (hostInfo.is_open())
+            {
+                while ( getline (hostInfo,line) )
+                {
+                    //REMOVE THIS LINE cout << line << '\n';
+                    // break the first line into IP
+                    // break the second line to Port No.
+                    string delimiter = ": ";
+                    string token = line.substr(1, line.find(delimiter));
+                }
+                hostInfo.close();
+            }
+            else cout << "Unable to open file";
+
+*/
+            //HostPlayer p3("127.0.0.1", 8000);
+            HostPlayer *p3 = new HostPlayer("127.0.0.1", 8000);
             try {
                 p3->connectToServer();
                 p3->getSymbolFromServer();
+                p2 = new TerminalPlayer(EnumDeclration::OtherPlayer(p3->getEnumSymbol()));
+               // p2 = new TerminalPlayer(p3->getEnumSymbol());
             } catch (const char *msg) {
                 cout << "Failed to connect to server. Reason:" << msg << endl;
             }
             p1 = p3;
             correct = true;
+            server = true;
         } else {//if input is taken or against the
             cout << "no such option. choose again" << endl;
         }
     } while (!correct);
     cin.ignore();
-    cout << "you are: 233" << p1->getEnumSymbol() << endl;
-    GameFlow gameFlow(&rl, b, p1, p2, false);
+    GameFlow gameFlow(&rl, b, p1, p2, server);
     gameFlow.Run();
 
     //delete members-free memory.
     delete b;
-    if (userChoice == 2 || userChoice == 1) {
-        delete p2;
-    }
+    delete p2;
     delete p1;
     return 0;
 }
